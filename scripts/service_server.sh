7b214bb6e46f985091bd2ed34263b856e47dbae0
sivakumarkatam <siva.katam@blazeclan.com>
2018-03-27 17:01:30 +0530

    Create service_server.sh

---
+++ scripts/service_server.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#document_root=/var/www/dev/magento
+deployment_root=/opt/deployment
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "unicorn-dev-cron" ]
+then
+    document_root=/var/www/devcron/magento    
+	  sudo cp -f -r $deployment_root/* /var/tmp/
+	  sudo cd $document_root
+	  sudo php bin/magento maintenance:enable
+    sudo rm -rf $document_root/*
+    sudo mkdir -p $document_root
+	  sudo cd $document_root
+	  sudo php bin/magento maintenance:enable
+    sudo cp -f -r $deployment_root/* $document_root/
+    sudo mkdir -p /run/php/
+    sudo chown -R nginx:nginx /run/php/
+  elif [ "$DEPLOYMENT_GROUP_NAME" == "unicorn-stg-cron" ]
+then
+	document_root=/var/www/stgcron/magento	
+	sudo cp -f -r $deployment_root/* /var/tmp/
+	sudo rm -rf $document_root/*
+	sudo mkdir -p $document_root
+	sudo cp -f -r $deployment_root/* $document_root/
+	sudo mkdir -p /run/php/
+	sudo chown -R nginx:nginx /run/php/
+	sudo systemctl start php-fpm
+	sudo systemctl enable php-fpm
+	sudo cp -f $deployment_root/scripts/StgEnv/env.php $document_root/app/etc/
+
+else 
+  echo "running else condition"
+fi
