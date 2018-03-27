7b5828410c3a7c225e033c8feb648bccc84401ac
sivakumarkatam <siva.katam@blazeclan.com>
2018-03-27 18:13:21 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,23 +4,25 @@ echo $DEPLOYMENT_GROUP_NAME
 echo "from start Server sh file"
 if [ "$DEPLOYMENT_GROUP_NAME" == "unicorn-dev-cron" ]
 then
-echo "enter to if condition for dev -cron"
-echo $DEPLOYMENT_GROUP_NAME
-document_root=/var/www/devcron/magento
-pwd
-cd $document_root
-echo "current directory"
-composer update
-php bin/magento deploy:mode:set developer
-php bin/magento setup:upgrade
-php bin/magento setup:di:compile
-php bin/magento setup:static-content:deploy -f
+	echo "start file execution is started"
+	echo "enter to if condition for dev -cron"
+	echo $DEPLOYMENT_GROUP_NAME
+	document_root=/var/www/devcron/magento
+	pwd
+	cd $document_root
+	echo "current directory"
+	composer update
+	php bin/magento deploy:mode:set developer
+	php bin/magento setup:upgrade
+	php bin/magento setup:di:compile
+	php bin/magento setup:static-content:deploy -f
 
-sudo chmod 700 $document_root/app/etc
-sudo chown -R nginx:nginx $document_root
-sudo find . -type f -exec chmod -c 644 {} \; && sudo find . -type d -exec chmod -c 755 {} \;
+	sudo chmod 700 $document_root/app/etc
+	sudo chown -R nginx:nginx $document_root
+	sudo find . -type f -exec chmod -c 644 {} \; && sudo find . -type d -exec chmod -c 755 {} \;
 
-sudo chown -R nginx:nginx *
+	sudo chown -R nginx:nginx *
+	echo "start file execution is done"
 elif [ "$DEPLOYMENT_GROUP_NAME" == "Unicorn-stg-cron" ]
 then
 document_root=/var/www/stgcron/magento2
