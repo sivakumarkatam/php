69afc2b65276dc99678eaece8061611214010117
sivakumarkatam <siva.katam@blazeclan.com>
2018-03-28 13:04:21 +0530

    Update stopApp.bash

--- scripts/stopApp.bash
+++ scripts/stopApp.bash
@@ -5,3 +5,8 @@ if [[ -n  $isExistApp ]]; then
    sudo service apache2 stop        
 fi
 echo "stop server is done"
+echo $DEPLOYMENT_GROUP_NAME
+if [ "$DEPLOYMENT_GROUP_NAME" == "unicorn-dev-cron" ]
+then
+echo "started stop app file in if condition"
+fi
