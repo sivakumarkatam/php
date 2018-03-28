af42ad94e3f8f1cc195e032c174eb54cd61064ce
sivakumarkatam <siva.katam@blazeclan.com>
2018-03-28 12:18:25 +0530

    Update and rename service_server.sh to serviceApp.bash

---
+++ scripts/serviceApp.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+document_root_cron_dev=/var/www/devcron/magento
+deployment_root=/opt/deployment
+
+document_root=/var/www/magento2
+#deployment_root=/opt/deployment
+if [ "$DEPLOYMENT_GROUP_NAME" == "Unicorn-Magento-Admin-LT" ]
+then
+	sudo cp -f -r $deployment_root/* /var/tmp/
+	sudo rm -rf $document_root/*
+	sudo mkdir -p $document_root
+	sudo cp -f -r $deployment_root/* $document_root/
+	sudo mkdir -p /run/php/
+	sudo chown -R nginx:nginx /run/php/
+	sudo systemctl start php-fpm
+	sudo systemctl enable php-fpm
+	sudo cp -f $deployment_root/scripts/LoadEnv/env.php $document_root/app/etc/
+
+
+elif [ "$DEPLOYMENT_GROUP_NAME" == "unicorn-dev-cron" ]
+then
+	echo "started service app file in if condition"
+	sudo cp -f -r $deployment_root/* /var/tmp/
+	sudo rm -rf $document_root_cron_dev/*
+	sudo mkdir -p $document_root_cron_dev
+	sudo cp -f -r $deployment_root/* $document_root_cron_dev/
+	sudo mkdir -p /run/php/
+	sudo chown -R nginx:nginx /run/php/
+	sudo systemctl start php-fpm
+	sudo systemctl enable php-fpm
+	sudo cp -f $deployment_root/scripts/DevEnv/env.php $document_root_cron_dev/app/etc/
+	echo "completed service app file in if condition"
+
+
+else 
+	exit 1
+
+fi
